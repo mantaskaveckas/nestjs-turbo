0b656c6e45fbbdd07e3f8f5e13c60df67f9bed85
Mantas Kaveckas <mantas.kaveckas@gmail.com>
2023-11-20 22:54:33 +0200

    adds react query devtools

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -10,6 +11,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
